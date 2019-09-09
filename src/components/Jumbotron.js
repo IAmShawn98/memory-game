ef286568502e18589d4d97c676b7f7965828b050
icepants <shawnluther99@gmail.com>
2019-09-09 00:36:24 -0400

    Performed JSX Cleanup, commented unexplained code so it's essier to read through.

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,11 +1,17 @@
+// All Imports.
 import React from "react";
 
+// Jumbotron Component.
 function Jumbotron() {
   return (
+    // Component Container.
     <div>
+      {/* JSX Jumbotron Container */}
       <div className="jumbotron bg text-white text-center">
         <div className="container">
+          {/* JSX Jumbotron Content */}
           <h1 className="display-3 font-weight-bold mb-5">Clicky Game!</h1>
+          {/* JSX Game Description */}
           <p className="lead">
             Welcome to the clicky memory game! Your objective is to click on any of the
             tiles containing images, memorizing which image you clicked on. Avoid
@@ -13,9 +19,11 @@ function Jumbotron() {
             and loose all of your points. Rack up points by clicking on as many
             unique images as possible, and secure the highest top score, good luck!
             </p>
+          {/* JSX Reset Game Session Icon */}
           <div className="container pl-5 mt-3">
             <ul className="navbar-nav">
               <li className="nav-item active">
+                {/* JSX Icon Describer */}
                 <p class="lead font-weight-bold text-info p-0 m-0">Reset Game Session</p>
                 <a className="nav-link text-white" href="#"><h2 className="display-5 text-white">
                   <i id="restartIcon" className="fa fa-refresh text-info"></i>
@@ -30,4 +38,5 @@ function Jumbotron() {
   );
 }
 
+// Do Component Export.
 export default Jumbotron;
